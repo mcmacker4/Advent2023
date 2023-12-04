5582236764a48228d6d72d487572ff382be8e02c
Hans Geel <mcmacker4@gmail.com>
2023-12-04 16:38:39 +0100

    Day 1

---
+++ src/bin/d1p1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let input = include_str!("./inputs/d1-input.txt");
+
+    let mut result = 0;
+
+    for line in input.lines() {
+        let first_digit = line
+            .chars()
+            .find_map(|c| c.to_digit(10))
+            .expect("Digit not found in line");
+        let last_digit = line
+            .chars()
+            .rev()
+            .find_map(|c| c.to_digit(10))
+            .expect("Digit not found in line");
+        result = result + (first_digit * 10 + last_digit);
+    }
+
+    println!("{}", result);
+}
---
+++ src/bin/d1p2.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let input = include_str!("./inputs/d1-input.txt");
+
+    let mut result = 0;
+
+    for line in input.lines() {
+        let first = find_digit_left(line).expect("Could not find left");
+        let last = find_digit_right(line).expect("Could not find right");
+
+        result = result + first * 10 + last;
+    }
+
+    println!("{}", result);
+}
+
+fn find_digit_left(input: &str) -> Option<u32> {
+    for i in 0..input.len() {
+        let part = &input[i..];
+        if let Some(num) = to_digit(part) {
+            return Some(num);
+        }
+    }
+    None
+}
+
+fn find_digit_right(input: &str) -> Option<u32> {
+    for i in (0..input.len()).rev() {
+        let part = &input[i..];
+        if let Some(num) = to_digit(part) {
+            return Some(num);
+        }
+    }
+    None
+}
+
+const PATTERNS: [&str; 10] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn to_digit(input: &str) -> Option<u32> {
+    if let Some(first_char) = input.chars().next() {
+        if let Some(n) = first_char.to_digit(10) {
+            return Some(n);
+        }
+    }
+
+    for (i, pat) in PATTERNS.iter().enumerate() {
+        if input.starts_with(pat) {
+            return Some(i as u32);
+        }
+    }
+    None
+}
